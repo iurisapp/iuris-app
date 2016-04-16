1c406237b0608018b5de3411f14aac5e1926ad9c
liviamorim <liviavas.amorim@gmail.com>
2016-04-16 13:09:57 -0300

    Fazendo o primeiro commit do Iuris app

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# iuris-app
+# Sobre o Iuris
 O Iuris é um mapa colaborativo gratuito que exibe rotas amigáveis para diferentes indivíduos. Foi criado para pedestres que sentem dificuldade em andar nas calçadas da cidade e querem descobrir a melhor opção de caminho para suas individualidades.
+
+## Tecnologias utilizadas
+Usamos o aplicativo App Inventor para fazer o aplicativo.
---
+++ src/appinventor/ai_iurisapp1/IURIS_copy/mapa.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"YaVersion":"150","Source":"Form","Properties":{"$Name":"mapa","$Type":"Form","$Version":"19","AppName":"IURIS","Sizing":"Responsive","Title":"Mapa Colaborativo","TitleVisible":"False","Uuid":"0","$Components":[{"$Name":"GoogleMaps","$Type":"ActivityStarter","$Version":"6","Action":"android.intent.action.VIEW","ActivityClass":"com.google.android.maps.MapsActivity","ActivityPackage":"com.google.android.apps.maps","Uuid":"1624023007"},{"$Name":"LocalAtual","$Type":"LocationSensor","$Version":"2","Uuid":"1741352928"}]}}
+|#
